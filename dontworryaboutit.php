8fa728563dfb173f01841aea1ea38313ac34c62c
jimmy white <45783163+jimmywhite999@users.noreply.github.com>
2023-05-25 11:21:00 -0400

    Update dontworryaboutit.php

--- dontworryaboutit.php
+++ dontworryaboutit.php
@@ -25,7 +25,7 @@ if (mysqli_num_rows($result) > 0) {
     header("Location: dashboard.html");
     exit();
 } else {
-    header("Location: login.html");
+    header("Location: index.html");
     exit();
 }
 ?>
